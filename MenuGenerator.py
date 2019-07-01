317be1c4ef6b5e254c49a3d9fb9b368d878c9fe2
epan1030 <52299671+epan1030@users.noreply.github.com>
2019-07-01 09:34:33 -0700

    Add files via upload

---
+++ MenuGenerator.py
@@ -0,0 +1,16 @@
+# #imports the ability to get a random number (we will learn more about this later!)
+# from random import *
+# #Create the list of words you want to choose from.
+# aListMainCourse = ["Hamburger", "Pizza", "Pasta", "Salad"]
+# bListSides = ["Fries", "Onion Rings", "Mashed Potatoes", "Bread Slices"]
+#
+# #Generates a random integer.
+# aRandomIndex = randint(0, len(aListMainCourse)-1)
+# bRandomIndex = randint(0, len(bListSides)-1)
+# print(aListMainCourse[aRandomIndex])
+# print(bListSides[aRandomIndex])
+
+ages=[5,12,3,56,24,78,1,15,44]
+sum=sum(ages)
+divide=sum/len(ages)
+print(divide)
